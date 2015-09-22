6b2dbae8a154e7523e4874752a470b3d4c0934b9
Josh West <joshwest@theatlantic.com>
2015-09-22 13:20:12 -0400

    Fixed #17665 — 'smart if' template tag now raises some exceptions
    Previously, all exceptions that occurred within a `{% if ... %}`
    template tag were swallowed and `False` returned. `TypeError`,
    `AttributeError`, `KeyError`, `ValueError`, and `IndexError` as well
    as exceptions where `silent_variable_failure == True` are handled as
    before, but all other exceptions are raised to the user.
    
    This error handling mirrors that in `django.template.base.Variable`

--- django/template/smartif.py
+++ django/template/smartif.py
@@ -56,11 +56,16 @@ def infix(bp, func):
         def eval(self, context):
             try:
                 return func(context, self.first, self.second)
-            except Exception:
+            except (TypeError, AttributeError, KeyError, ValueError, IndexError):
                 # Templates shouldn't throw exceptions when rendering.  We are
                 # most likely to get exceptions for things like {% if foo in bar
                 # %} where 'bar' does not support 'in', so default to False
                 return False
+            except Exception as e:
+                if getattr(e, 'silent_variable_failure', False):
+                    return False
+                else:
+                    raise
 
     return Operator
 
@@ -81,8 +86,13 @@ def prefix(bp, func):
         def eval(self, context):
             try:
                 return func(context, self.first)
-            except Exception:
+            except (TypeError, AttributeError, KeyError, ValueError, IndexError):
                 return False
+            except Exception as e:
+                if getattr(e, 'silent_variable_failure', False):
+                    return False
+                else:
+                    raise
 
     return Operator
 
